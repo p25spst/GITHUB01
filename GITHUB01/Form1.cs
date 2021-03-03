95f61d90291267a6b01bf82698c94021af9612b3
p25.test <pisemka1.test@gmail.com>
2021-03-03 15:21:59 +0100

    Pridan rozdil

---
+++ GITHUB01/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GITHUB01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = int.Parse(textBox1.Text);
+            double b = int.Parse(textBox2.Text);
+            double soucet = a + b;
+            double rozdil = a - b;
+            MessageBox.Show("Součet je " + soucet);
+            MessageBox.Show("Rozdil je " + rozdil);
+        }
+    }
+}
